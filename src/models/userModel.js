ed01460935d769a279975aea302822748233b6bc
smile-namrata <namrata.jaiswal102441@gmail.com>
2022-07-06 12:51:13 +0530

    j

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
 
-    title:{
-        type:String;
-        
-    }
+    title: { type:String, required:true, enum:["Mr","Mrs","Miss"],
+
+    name: { type:String, required:true},
+
+    phone: { type:String, required:true, unique:true},
+
+    email: { type:String, required:true, unique:true, },
+
+    password:
+       
+
+
+    
 })
 
  
